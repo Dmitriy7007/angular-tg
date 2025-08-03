bafa1a62e5d0cd05c3293d5582deff98b3dd5d0d
Dmitriy7007 <cherhan86@gmail.com>
2025-08-03 13:10:03 +0700

    added styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,20 @@
 import { Routes } from '@angular/router';
+import { Shop } from './pages/shop/shop';
+import { Product } from './pages/product/product';
+import { Feedback } from './pages/feedback/feedback';
 
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/shop/shop').then((m) => m.Shop),
+    component: Shop,
     pathMatch: 'full',
   },
   {
     path: 'product/:id',
-    loadComponent: () =>
-      import('./pages/product/product').then((m) => m.Product),
+    component: Product,
   },
   {
     path: 'feedback',
-    loadComponent: () =>
-      import('./pages/feedback/feedback').then((m) => m.Feedback),
+    component: Feedback,
   },
 ];
